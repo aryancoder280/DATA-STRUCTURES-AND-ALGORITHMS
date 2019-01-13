b6deee6d88ed7ddd32331e3c898ee72c9d8f10f2
aryancoder280 <41503577+aryancoder280@users.noreply.github.com>
2019-01-14 02:02:28 +0530

    Update To Determine if Two Trees are Identical_RECURSIVE.cpp

--- BASIC QUESTIONS ON TREES/To Determine if Two Trees are Identical_RECURSIVE.cpp
+++ BASIC QUESTIONS ON TREES/To Determine if Two Trees are Identical_RECURSIVE.cpp
@@ -1,11 +1,12 @@
-/*ASSUME ROOTS OF TWO NODES IS PASSED AS AN ARGUMENT IN THE FUNCTION*/
-bool check(node*r1,node*r2)
+/*ASSUME ROOT OF TWO TREES ARE PASSED AS  ARGUMENTS IN THE FUNCTION*/
+bool check(node*r1 , node*r2)
 {
     //base case
     if(r1==NULL&&r2==NULL)
     {
         return 1;
     }
+    //RECURSIVE CASE
     else if(r1!=NULL&&r2!=NULL)
     {
         return (r1->data==r2->data&&check(r1->left,r2->left)&&check(r1->right,r2->right));
